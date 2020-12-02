08859328f49494387f9fafdc71fd6806b8846c66
alinaromanenko <alinaololoevaone@gmail.com>
2020-12-02 20:03:34 +0700

    Raw form

--- src/main/java/NSU/ui/Item.java
+++ src/main/java/NSU/ui/Item.java
@@ -25,7 +25,7 @@ public class Item {
 
 	private Long id;
 
-	@NotEmpty(message = "Name is required.")
+	@NotEmpty(message = "Введите название товара.")
 	private String name;
 
 	private String description;
